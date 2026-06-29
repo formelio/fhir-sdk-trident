4b6042c4ad8521ca97fd0085461b51bcd431354a
Jake Jongejans <40899982+Javernus@users.noreply.github.com>
2026-06-29 09:04:21 +0200

    feat: no-fhir

--- crates/fhir-model/README.md
+++
@@ -1,3 +0,0 @@
-# FHIR SDK Models
-
-This is a sub-crate of [`fhir-sdk`](https://crates.io/crates/fhir-sdk), please take a look there for more information.
--- crates/fhir-sdk/src/utils.rs
+++
@@ -1,5 +0,0 @@
-//! Utils for internal use (outside client).
-
-/// Crate internal trait to disallow users to implement some trait externally.
-/// Do not publicly export :D
-pub trait Sealed {}
